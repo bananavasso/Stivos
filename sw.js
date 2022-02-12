1335347ef048f0220f5746c2bfc80da7e6ef0c7a
bananavasso <81524536+bananavasso@users.noreply.github.com>
2022-02-12 16:41:59 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ self.addEventListener('install', function (event) {
                 '/VasilikiStavrou.html',
                 '/VasilikiStavrou.css',
                 '/uomTrack.js',
-                '/install.js'
+                '/index.js'
             ]);
         })
     );
